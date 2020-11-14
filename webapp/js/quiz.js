028ec3428845cf19238e9bcd17b201a21aca14d4
nick <nickbenz27@gmail.com>
2020-11-13 22:38:53 -0800

    added onload local storage checks for questions, answers, and counter

--- webapp/js/quiz.js
+++ webapp/js/quiz.js
@@ -1,14 +1,85 @@
+let pgQuestionWords;
 let questionWords;
+let questionWordsStringified;
+const quizStorage = localStorage;
+let questionCounter;
+let answers;
 
-window.onload = function(e){ 
-    fetch("/charts/listAllQuestionWords")
-    .then(response => response.json())
-    .then(response => {questionWords = response.rows;})
-    .then(()=>{
-        console.log(questionWords)
-    });
+window.onload = function(){ 
+    checkQuizHistory();
+}
+
+
+const checkQuizHistory = ()=>{
+    checkForQuestions();
+    checkForCounter();
+    checkForAnswerValues();
+}
+
+const checkForCounter = ()=>{
+ if(!localStorage.getItem("counter")){
+    questionCounter = 177;
+    localStorage.setItem("counter", questionCounter);
+ } else {
+    questionCounter = parseInt(localStorage.getItem("counter"));
+ }
+}
+const checkForAnswerValues = ()=>{
+    if(!localStorage.getItem("answers")){
+        answers = {
+            "white" : 0,
+            "blue" : 0,
+            "black" : 0,
+            "red" : 0,
+            "green" : 0
+        }
+        localStorage.setItem("answers", JSON.stringify(answers));
+     } else {
+        answers = JSON.parse(localStorage.getItem("answers"));
+     }
+}
+
+const checkForQuestions = ()=>{
+    if(!localStorage.getItem("questions")){
+        fetch("/charts/listAllQuestionWords")
+        .then(response => response.json())
+        .then(response => {pgQuestionWords = response.rows;})
+        .then(()=>{
+            shuffle(pgQuestionWords);
+            questionWordsStringified = JSON.stringify(pgQuestionWords);
+            localStorage.setItem("questions", questionWordsStringified);
+            questionWords = pgQuestionWords;
+        })
+    } else {
+        questionWords = JSON.parse(localStorage.getItem("questions"));
+    }
 }
 
 const selected = (responseValue) => {
-    alert(responseValue)
+    alert(`(${questionCounter})  ${questionWords[questionCounter].question}, ${questionWords[questionCounter].cd_color}  :::   ${responseValue}`);
+    
+    if(questionCounter < questionWords.length -1){
+        questionCounter += 1;
+    }
+
+    localStorage.setItem("counter", questionCounter);
 }
+
+
+
+
+const shuffle = (array) => {
+    // https://bost.ocks.org/mike/shuffle/
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
